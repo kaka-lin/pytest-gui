16dab0054fd9707285798672352cd2261d5743f5
kaka <vn503024@gmail.com>
2017-10-26 16:45:21 +0800

    Add: Abort Thread 雛形

--- gui/testareabox.py
+++ gui/testareabox.py
@@ -4,7 +4,10 @@ import queue
 import pytest
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot, QTime, QTimer
+from PyQt5.QtWidgets import QApplication
 from gui.ui_testareabox import Ui_TestAreaBox
+
+origin_stdout = ''
 
 class WriteStream(QObject):
     trigger = pyqtSignal()
@@ -37,17 +40,35 @@ class Worker(QObject):
         super(Worker, self).__init__(parent)
 
         self.path = path
+
         self.queue = queue.Queue()
         self.writeStream = WriteStream(self.queue)
+
+        self.__abort = False
+        
         
     @pyqtSlot()
     def work(self):
+        global origin_stdout
         origin_stdout = sys.stdout
         self.writeStream.trigger.connect(self.on_item_change)
         sys.stdout = self.writeStream
         pytest.main(['-p', 'no:terminal'])
 
+        '''
+        for step in range(100):
+            time.sleep(0.1)
+            print(step)
+                
+            QApplication.processEvents()
+            if self.__abort:
+                break
+        '''
+        
         self.sig_done.emit()
+    
+    def abort(self):
+        self.__abort = True
     
     def on_item_change(self):
         while True:
@@ -61,7 +82,10 @@ class Worker(QObject):
             else:
                 self.sig_msg.emit(line)
             
-class TestAreaBox(QtWidgets.QGroupBox): 
+class TestAreaBox(QtWidgets.QGroupBox):
+
+    sig_abort_workers = pyqtSignal()
+
     def __init__(self, parent=None):
         super(TestAreaBox, self).__init__(parent)
 
@@ -83,6 +107,7 @@ class TestAreaBox(QtWidgets.QGroupBox):
     
     def on_run_test_button_clicked(self):
         """ Pytest Thread Start """
+        self.ui.list.clear()
 
         self.__threads = []
         worker = Worker()
@@ -93,16 +118,28 @@ class TestAreaBox(QtWidgets.QGroupBox):
         worker.sig_done.connect(self.on_worker_done)
         worker.sig_msg.connect(self.show_info)
 
+        # control worker:
+        self.sig_abort_workers.connect(worker.abort)
+
         thread.started.connect(worker.work)
         thread.start()
     
     def on_stop_test_button_clicked(self):
-        """ """
-
-    def on_worker_done(self):
+        '''
+        self.sig_abort_workers.emit()
         for thread, Worker in self.__threads:
             thread.quit()
             thread.wait()
+        sys.stdout = origin_stdout
+        print('Pytest Thread Abort!')
+        '''
+
+    def on_worker_done(self):
+        sys.stdout = origin_stdout
+        for thread, Worker in self.__threads:
+            thread.quit()
+            thread.wait()
+        print('Pytest Thread Finished!')
 
     def show_info(self, line):
         self.ui.list.insertPlainText(line)
